993f76915ee874471d5c8b02ac3ab4cb069b1f4c
Leon in 't Veld <leon3110l@gmail.com>
2020-04-10 03:03:25 +0200

    added import

--- katana/pedals/__init__.py
+++ katana/pedals/__init__.py
@@ -1,4 +1,4 @@
-from .pedal import BasePedal, FXPedal, FXType
+from .pedal import BasePedal, FXPedal, FXType, Pedal
 from .amp import Amp, AmpType
 from .booster import Booster, BoosterType
 from .delay import Delay, DelayType
@@ -9,3 +9,5 @@ from .reverb import ReverbType, Reverb
 from .sr import SendReturn, SendReturnType
 from .volume import Volume, VolumeType
 from .wah import Wah, WahSubType, WahType
+
+from .effects import *
